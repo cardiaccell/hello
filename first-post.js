45f3ac7db000f11a00686aa9fd37fa081c338a36
cardiaccell <105444416+cardiaccell@users.noreply.github.com>
2022-05-13 11:54:38 +0900

    Create first-post.js

---
+++ first-post.js
@@ -0,0 +1,11 @@
+export default function FirstPost() {
+    return (
+        <div>
+            <h1>My information</h1>
+            <p>My name is 윤종수 and my hobby is playing games, and I like chocolates.</p>
+        </div>
+        )
+        
+
+    
+}
